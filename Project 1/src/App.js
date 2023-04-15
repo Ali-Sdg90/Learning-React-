6a2f59d78ba89e83d7a38ef77370c3213d47ec32
Ali Sadeghi <ali909392@gmail.com>
2023-04-15 22:31:46 +0330

    Add Navbar for Project 1

--- Project 1/src/App.js
+++ Project 1/src/App.js
@@ -1,13 +1,10 @@
-import React, { Component } from 'react';
-import "./App.css"
+import React, { Component } from "react";
+
+import Navbar from "./components/Navbar";
 
 class App extends Component {
     render() {
-        return (
-            <div>
-                <h1>Hello</h1>
-            </div>
-        );
+        return <Navbar />;
     }
 }
 
