6c95aa24b298781b2439127cbd1f38ee69a14738
Ali Sadeghi <ali909392@gmail.com>
2024-01-28 14:41:49 +0330

    Add usecontext examples

--- Practice-React1/practice01/src/App.tsx
+++ Practice-React1/practice01/src/App.tsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState, createContext } from "react";
 import NumberCounter from "./NumberCounter";
+import Context1 from "./Context-Folder/Context1";
+
+export const ThemeContext = createContext({
+    theme: "info",
+    setTheme: (theme: string) => {},
+});
 
 const App = () => {
+    const [theme, setTheme] = useState("info");
+
     return (
         <div>
             <NumberCounter />
+            <NumberCounter />
+
+            <ThemeContext.Provider value={{ theme, setTheme }}>
+                <Context1 />
+            </ThemeContext.Provider>
         </div>
     );
 };
